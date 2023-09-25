dd8263ab28609bf31289f584cb0448cba38a7c89
Leonid Lavrov <274852@gmail.com>
2023-09-25 22:29:10 +0300

    adding meals list

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meals_list.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
@@ -13,11 +14,37 @@ class MealsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Uh oh ... nothing here!',
+            style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          ),
+          const SizedBox(height: 16),
+          Text(
+            'Try selecting a different category!',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.onBackground,
+                ),
+          )
+        ],
+      ),
+    );
+
+    if (meals.isNotEmpty) {
+      mainContent = MealsList(meals: meals);
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: ,
+      body: mainContent,
     );
   }
 }
